0588823bf708d81d204ac34d6ff5639c780c5a22
Kavya <Kavya@DESKTOP-AJA0JKM>
2022-11-22 10:47:33 +0530

    practise

---
+++ src/main/java/practise/Sample1Test.java
@@ -0,0 +1,29 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+
+
+	@Test
+	public void script1()
+	{
+		System.out.println("script1");
+	}
+	
+	@Test
+	public void script2()
+	{
+		System.out.println("script2");
+	}
+	@Test
+	public void script3()
+	{
+		System.out.println("script3");
+	}
+	@Test
+	public void script4()
+	{
+		System.out.println("script3");
+	}
+}
---
+++ src/main/java/practise/Sample5.java
@@ -0,0 +1,8 @@
+package practise;
+
+public class Sample5 {
+public static void main(String[] args) {
+	
+	System.out.println("Hi");
+}
+}
